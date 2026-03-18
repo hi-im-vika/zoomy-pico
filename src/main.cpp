3c85572caa160aa6b813058e8386aa17886769f2
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-03-18 14:51:27 -0700

    Init Wire object

--- src/main.cpp
+++ src/main.cpp
@@ -44,6 +44,12 @@ void setup() {
   Serial1.setTX(0);
   Serial1.setRX(1);
   Serial1.begin(115200);
+
+  Wire.setSDA(4);
+  Wire.setSCL(5);
+  Wire.begin();
+  
+  u8g2.begin();
 }
 
 void loop() {
