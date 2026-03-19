aaca44e0af71f2ab2398542926596086ed91a6c0
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-03-19 15:55:34 -0700

    Add Console class to scroll text on OLED

---
+++ include/Console.hpp
@@ -0,0 +1,17 @@
+#include <U8g2lib.h>
+
+class Console {
+    public:
+    Console() = default;
+    ~Console();
+    void init(U8G2 *u);
+    void add(char *str);
+    void display();
+
+    private:
+    char **_lines;
+    U8G2 *_u8g2;
+    uint8_t _num_lines;
+    uint8_t _width_line;
+    uint8_t _height_line;
+};
---
+++ src/Console.cpp
@@ -0,0 +1,35 @@
+#include "Console.hpp"
+
+Console::~Console() {
+    free(_lines);
+}
+
+void Console::init(U8G2 *u) {
+    _u8g2 = u;
+    _num_lines = u->getDisplayHeight() / u->getMaxCharHeight();
+    _width_line = u->getDisplayWidth() / u->getMaxCharWidth();
+    _height_line = u->getMaxCharHeight();
+
+
+    _lines = (char**) malloc(_num_lines * sizeof(char*));
+    for (int i = 0; i < _num_lines; i++) {
+        *(_lines + i) = (char*) malloc(_width_line * sizeof(char));
+    }
+}
+
+void Console::add(char *str) {
+    for (int i = _num_lines - 1; i--; i < 0) {
+        strcpy(_lines[i+1], _lines[i]);
+    }
+    strcpy(_lines[0], str);
+}
+
+void Console::display() {
+    _u8g2->clearBuffer();
+
+    for (int i = 0; i < _num_lines; i++) {
+        _u8g2->setCursor(0,_height_line * i);
+        _u8g2->print(_lines[i]);
+    }
+    _u8g2->sendBuffer();
+}
