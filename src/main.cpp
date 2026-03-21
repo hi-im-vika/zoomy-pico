50e3c0c2944e972ff01c40ee77da1a9d51e98bbc
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-03-21 00:25:16 -0700

    Re-enable radio power limit

--- src/main.cpp
+++ src/main.cpp
@@ -151,7 +151,7 @@ void setup() {
   console.append("OK");
   console.display();
 
-  // radio.setPALevel(RF24_PA_LOW);  // RF24_PA_MAX is default.
+  radio.setPALevel(RF24_PA_LOW);  // RF24_PA_MAX is default.
   radio.setPayloadSize(sizeof(payload));  // float datatype occupies 4 bytes
   radio.openReadingPipe(1, address);  // using pipe 1
   radio.startListening();
