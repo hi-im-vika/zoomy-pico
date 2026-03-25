214af7b8afb93729a28ed446ddf150ff56e833a6
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-03-25 02:27:44 -0700

    Add basic PCA9685 code into namespaced file

--- include/PCA9685.hpp
+++ include/PCA9685.hpp
@@ -4,5 +4,7 @@
 #include <Adafruit_PWMServoDriver.h>
 
 namespace PCA9685 {
-    
+    void init();
+    void setPWM(uint8_t num, uint16_t on, uint16_t off);
+    void stopAll();
 }
--- src/PCA9685.cpp
+++ src/PCA9685.cpp
@@ -1,9 +1,24 @@
 #include "PCA9685.hpp"
 
 namespace {
-
+    Adafruit_PWMServoDriver pwm;
 }
 
 namespace PCA9685 {
-    
+    void init() {
+        pwm = Adafruit_PWMServoDriver(0x40, Wire);
+        pwm.begin();
+        stopAll();
+        // pwm.setPWMFreq(1000);
+    }
+
+    void setPWM(uint8_t num, uint16_t on, uint16_t off) {
+        pwm.setPWM(num, on, off);
+    }
+
+    void stopAll() {
+        for (int i = 0; i < 16; i++) {
+            pwm.setPWM(i, 0, 4096);
+        }
+    }
 }
